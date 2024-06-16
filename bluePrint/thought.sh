7d4b13054fce863586824020c75a14596c73eed3
ImRecc <46367069+ImRecc@users.noreply.github.com>
2024-06-16 17:42:58 +0900

    Update thought.sh

--- bluePrint/thought.sh
+++ bluePrint/thought.sh
@@ -1,2 +1,6 @@
 this is about bp
 like even、variable and timeline or say "sequence of a bp"
+
+#base principal of workflow
+there is no traditional flow or time-based running sequence in blue print
+a custom event or a event just like a method only run when the time it been called
