f0184cde83efcd9d8d94444462f5ee092c5f3a1d
ImRecc <46367069+ImRecc@users.noreply.github.com>
2024-07-12 16:38:38 +0900

    Update thought.sh

--- bluePrint/thought.sh
+++ bluePrint/thought.sh
@@ -4,3 +4,7 @@ like even、variable and timeline or say "sequence of a bp"
 #base principal of workflow
 there is no traditional flow or time-based running sequence in blue print
 a custom event or a event just like a method only run when the time it been called
+
+I wanna talk about Animations
+we record some nice and fancy animation, then we create an AnimationMontage, 
+yes, AnimationMontage just like an array of different AnimationMontage, can be defined as a variable
